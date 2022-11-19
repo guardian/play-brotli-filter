ff9f5c9bd5020ac42aa56f09fedc2f99af765df3
Mariot Chauvin <mariot.chauvin@gmail.com>
2022-11-19 22:22:45 +0000

    Remove previous bad lib and add script to update

---
+++ scripts/update-lib.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd ../lib/
+
+version='1.8.0' 
+
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-aarch64/${version}/native-osx-aarch64-${version}.jar"
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-x86_64/${version}/native-osx-x86_64-${version}.jar"
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-windows-x86_64/${version}/native-windows-x86_64-${version}.jar"
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-x86_64/${version}/native-linux-x86_64-${version}.jar"
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-aarch64/${version}/native-linux-aarch64-${version}.jar"
