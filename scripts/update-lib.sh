ad3b0bcb55ce39c4176a16fac7545b9972e61003
Mariot Chauvin <mariot.chauvin@gmail.com>
2022-12-30 13:33:18 +0000

    Update to latest brotli4j version

--- scripts/update-lib.sh
+++ scripts/update-lib.sh
@@ -2,10 +2,11 @@
 
 cd ../lib/
 
-version='1.8.0' 
+version='1.9.0' 
 
 curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-aarch64/${version}/native-osx-aarch64-${version}.jar"
 curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-x86_64/${version}/native-osx-x86_64-${version}.jar"
 curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-windows-x86_64/${version}/native-windows-x86_64-${version}.jar"
 curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-x86_64/${version}/native-linux-x86_64-${version}.jar"
 curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-aarch64/${version}/native-linux-aarch64-${version}.jar"
+curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-armv7/${version}/native-linux-armv7-${version}.jar"
