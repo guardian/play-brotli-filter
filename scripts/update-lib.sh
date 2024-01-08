9e2a5d98de58354bbd15d7e8708340af3450f840
Mariot Chauvin <mariot.chauvin@gmail.com>
2024-01-08 12:04:18 +0000

    Avoid repetition

--- scripts/update-lib.sh
+++ scripts/update-lib.sh
@@ -4,9 +4,12 @@ cd ../common/lib/
 
 version='1.12.0' 
 
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-aarch64/${version}/native-osx-aarch64-${version}.jar"
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-osx-x86_64/${version}/native-osx-x86_64-${version}.jar"
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-windows-x86_64/${version}/native-windows-x86_64-${version}.jar"
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-x86_64/${version}/native-linux-x86_64-${version}.jar"
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-aarch64/${version}/native-linux-aarch64-${version}.jar"
-curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/native-linux-armv7/${version}/native-linux-armv7-${version}.jar"
+for platform in 'native-osx-aarch64' \
+                'native-osx-x86_64' \
+                'native-windows-x86_64' \
+                'native-linux-x86_64' \
+                'native-linux-aarch64' \
+                'native-linux-armv7'
+do
+   curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/${platform}/${version}/${platform}-${version}.jar"
+done
