487a5d42a0fd03e1641adcf9bf0adefaa584df00
Mariot Chauvin <mariot.chauvin@gmail.com>
2024-01-08 12:07:12 +0000

    Update to Brotli4j 1.5

--- scripts/update-lib.sh
+++ scripts/update-lib.sh
@@ -2,14 +2,18 @@
 
 cd ../common/lib/
 
-version='1.12.0' 
+version='1.15.0'
 
 for platform in 'native-osx-aarch64' \
                 'native-osx-x86_64' \
                 'native-windows-x86_64' \
+                'native-windows-aarch64' \
                 'native-linux-x86_64' \
                 'native-linux-aarch64' \
-                'native-linux-armv7'
+                'native-linux-armv7' \
+                'native-linux-s390x' \
+                'native-linux-riscv64' \
+				'native-linux-ppc64le'	
 do
    curl  -OJ -# "https://repo1.maven.org/maven2/com/aayushatharva/brotli4j/${platform}/${version}/${platform}-${version}.jar"
 done
